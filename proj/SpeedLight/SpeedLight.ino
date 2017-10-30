90ed2112fe920e275a6c670adb4b8e30098b1b5b
Baran Usluel <me@baranusluel.com>
2017-10-30 17:14:35 -0400

    Changed for consistency with MotorShield.ino

--- proj/SpeedLight/SpeedLight.ino
+++ proj/SpeedLight/SpeedLight.ino
@@ -4,17 +4,23 @@
  * and compares to arbitrary speed setpoint value. Lights up LED when difference is larger than arbitrary threshold.
  */
 
-#define encoder0PinA  2
-#define encoder0PinB  3
-#define ledPin 13
-#define errorThreshold 20
-#define cpr 200 // Cycles per revolution of encoder disk
+const int encoder0PinA = 2;
+const int encoder0PinB = 3;
+const int ledPin = 13;
+const float deadband = 20.0; // Error threshold/deadband in ticks per second
 
-int targetSpeed = 40; // Target angulary velocity / setpoint in degrees per second
+// Following constants are taken from MotorShield.ino
+const float ticksPerRev = 200.0; // Cycles/ticks per revolution of encoder disk
+const float gearRatio = 32.0;
+const float wheelCir = 1.092; // Meters
+const float metersPerTick = wheelCir / (ticksPerRev * gearRatio);
 
-volatile long encoder0Pos = 0; // Delta of encoder position since last loop iteration
+float targetSpeed = 40; // Target velocity/setpoint in m/s
+float targetAngSpeed = targetSpeed / metersPerTick; // Target angular velocity/setpoint in ticks per second
+
+volatile int encoder0Pos = 0; // Delta of encoder position since last loop iteration
 long oldTime = 0; // Time in ms from last loop iteration
-int dps = 0; // Angular velocity in degrees per second
+float angSpeed = 0; // Angular velocity in ticks per second
 
 void setup() {
   pinMode(encoder0PinA, INPUT);
@@ -30,11 +36,11 @@ void setup() {
 }
 
 void loop() {
-  // Divide delta of position by 4*cpr (because 4X decoding of quadrature encoder)
-  // and multiply by 360 for change in degrees, divide by delta time, & convert from ms^-1 to s^-1.
-  dps = ((encoder0Pos) * 360 * 1000 / (4*cpr)) / (millis() - oldTime);
-  Serial.println(dps);
-  if (abs(dps - targetSpeed) > errorThreshold) {
+  // Divide delta of position by 4 (because 4X decoding of quadrature encoder)
+  // to get ticks from edges, divide by delta time & convert from ms^-1 to s^-1.
+  angSpeed = encoder0Pos * 1000 / 4.0 / (millis() - oldTime);
+  Serial.println(angSpeed);
+  if (abs(angSpeed - targetAngSpeed) > deadband) {
     digitalWrite(ledPin, HIGH);
   } else {
     digitalWrite(ledPin, LOW);

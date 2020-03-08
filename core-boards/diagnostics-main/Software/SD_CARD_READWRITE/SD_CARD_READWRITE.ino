e2637b68c0e9b256728ca5fd5fce85393f01266d
gbotkin3 <43323668+gbotkin3@users.noreply.github.com>
2020-03-08 18:51:18 -0400

    More work on Firmware

--- core-boards/diagnostics-main/Software/SD_CARD_READWRITE/SD_CARD_READWRITE.ino
+++ core-boards/diagnostics-main/Software/SD_CARD_READWRITE/SD_CARD_READWRITE.ino
@@ -1,3 +1,13 @@
+
+/*
+TO DO:
+
+1. SD Card Reading from can for all files
+3. Radio Firmware 
+
+*/
+
+
 /*
  * 
  * IGVC Diagnostic Board Code
@@ -6,18 +16,31 @@
  
 #include <SPI.h>
 #include <SD.h>
+#include <Canbus.h>
+#include <defaults.h>
+#include <global.h>
+#include <mcp2515.h>
+#include <mcp2515_defs.h>
 
 // Variables Used for the SD Card
+String TimeFromStartup;
 File Temp;
 String TempData;
 File CurrentSensor;
-String CurrentSpeedData;
+String CurrentSpeedData;                                                                                                                                                                                                                                                                                                                                                                      
 File LogicBoard;
 String LogicBoardData;
 File EstopStatus;
 String EstopStatusData;
 
-File myFile;
+
+
+
+
+
+
+
+
 
 void setup()
 {
@@ -26,25 +49,72 @@ void setup()
    while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
+
+  //Initialize the SD Card for reading and writing
   Serial.print("Initializing SD card...");
-   pinMode(19, OUTPUT);
+  pinMode(19, OUTPUT);
   if (!SD.begin(19)) {
     Serial.println("initialization failed!");
     return;
   }
   Serial.println("initialization done.");
+  
+  //Initialize the CAN Modules
+  Serial.println("CAN Read - Testing receival of CAN Bus message");  
+  if(Canbus.init(CANSPEED_500))  //Initialise MCP2515 CAN controller at the specified speed
+    Serial.println("CAN Init ok");
+  else
+    Serial.println("Can't init CAN");
 }
   
 
 void loop()
 {
+
+  //Enable SD Card
+  digitalWrite(19, LOW);
+  //Clear All Data
+  String TempData = "";
+  String CurrentSpeedData = "";                                                                                                                                                                                                                                                                                                                                                                      
+  String LogicBoardData = "";
+  String EstopStatusData = "";
+  
   // Temparature Sensor Data
+  
+  //obtain data from CAN here
+  tCAN message;
+  if (mcp2515_check_message()) 
+  {
+    if (mcp2515_get_message(&message)) 
+  {
+        //if(message.id == 0x620)  //uncomment when you want to filter
+             //{
+               
+               Serial.print("ID: ");
+               Serial.print(message.id,HEX);
+               Serial.print(", ");
+               Serial.print("Data: ");
+               Serial.print(message.header.length,DEC);
+               for(int i=0;i<message.header.length;i++) 
+                { 
+                  Serial.print(message.data[i],HEX);
+                  Serial.print(" ");
+                  //TempData = TempData + message.data[i]
+                }
+               Serial.println("");
+             //}
+           }}
+
+  TempData = "Test 1...2...3..";
+  
   // open the file
   Temp = SD.open("Temparature Sensor Log.txt", FILE_WRITE);  
   // if the file opened okay, write to it:
   if (Temp) {
     Serial.print("Writing to Temparature Sensor Log.txt...");
-    Temp.println("testing 1, 2, 3.");
+    TimeFromStartup = String(millis());
+    Temp.println("Time: " + TimeFromStartup + " Temparture(Celsius): ");
+    Temp.println(TempData);
   // close the file:
     Temp.close();
     Serial.println("done.");
@@ -54,12 +124,18 @@ void loop()
   }
 
     //Current Sensor Data
+
+    //obtain data here
+    CurrentSpeedData = "Test 1...2...3..";  
+    
     // open the file
   CurrentSensor = SD.open("Current Sensor Log.txt", FILE_WRITE);  
   // if the file opened okay, write to it:
   if (CurrentSensor) {
     Serial.print("Writing to Current Sensor Log.txt...");
-    CurrentSensor.println("testing 1, 2, 3.");
+    TimeFromStartup = String(millis());
+    CurrentSensor.println("Time: " + TimeFromStartup + " Current(Amps): ");
+    CurrentSensor.println(CurrentSpeedData);
   // close the file:
     CurrentSensor.close();
     Serial.println("done.");
@@ -69,12 +145,18 @@ void loop()
   }
 
     // Logic Board Data Log
+
+    //obtain data here
+    LogicBoardData = "Test 1...2...3..";
+    
     // open the file
   LogicBoard = SD.open("Logic Board Log.txt", FILE_WRITE);  
   // if the file opened okay, write to it:
   if (LogicBoard) {
     Serial.print("Writing to Logic Board Log.txt...");
-    LogicBoard.println("testing 1, 2, 3.");
+    TimeFromStartup = String(millis());
+    LogicBoard.println("Time: " + TimeFromStartup + " MbedStatus: ");
+    LogicBoard.println(LogicBoardData);
   // close the file:
     LogicBoard.close();
     Serial.println("done.");
@@ -84,12 +166,18 @@ void loop()
   }
 
     // Estop Status Log
+
+    //obtain data here
+    EstopStatusData = "Test 1...2...3..";
+    
     // open the file
   EstopStatus = SD.open("Estop Status Log.txt", FILE_WRITE);  
   // if the file opened okay, write to it:
   if (myFile) {
     Serial.print("Writing to Estop Status Log.txt...");
-    EstopStatus.println("testing 1, 2, 3.");
+    TimeFromStartup = String(millis());
+    EstopStatus.println("Time: " + TimeFromStartup + " EstopStatus: ");
+    EstopStatus.println(EstopStatusData);
   // close the file:
     EstopStatus.close();
     Serial.println("done.");
@@ -97,19 +185,7 @@ void loop()
     // if the file didn't open, print an error:
     Serial.println("error opening Estop Status Log.txt");
   }
-
-    //
-    // open the file
-  myFile = SD.open("test.txt", FILE_WRITE);  
-  // if the file opened okay, write to it:
-  if (myFile) {
-    Serial.print("Writing to test.txt...");
-    myFile.println("testing 1, 2, 3.");
-  // close the file:
-    myFile.close();
-    Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
+  
+  //Disable SD Card
+  digitalWrite(19, HIGH);
 }
